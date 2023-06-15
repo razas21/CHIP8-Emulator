6a4483cf694d9408febae9ef7399f8ce37ec5f53
razaa <razaa@XPS-ALI>
2023-06-15 00:39:38 -0400

    implemented most opcodes, fixed bugs

--- Chip8_Emulator/main.cpp
+++ Chip8_Emulator/main.cpp
@@ -8,21 +8,20 @@ using namespace std;
 #include <stack>
 
 
-unsigned char memory[4096] = { 0 }; //4096 bytes of RAM
-unsigned char registers[16]; //16 8-bit general purpose registers
+unsigned char memory[4096] = {0}; //4096 bytes of RAM
+unsigned char registers[16] = {0}; //16 8-bit general purpose registers
 char delay_timer; //is decremented at a rate of 60Hz until it reaches 0
 char sound_timer; //gives off a beeping sound as long as it’s not 0
 
-int PC; //program counter - stores the currently executing address
-char SP; //stack counter - used to point to the topmost level of the stack.
-int I; //index register - points at locations in memory
+int PC = 0; //program counter - stores the currently executing address
+char SP = 0; //stack counter - used to point to the topmost level of the stack.
+int I = 0; //index register - points at locations in memory
 stack<int> routine_stack; //Used to store the address that the interpreter should return to when finished with a subroutine
 
 int frequency = 700;
-int display[32][64] = { 0 }; 
-
 const int SCREEN_WIDTH = 64;
 const int SCREEN_HEIGHT = 32;
+int display[SCREEN_HEIGHT][SCREEN_WIDTH] = { 0 }; 
 
 SDL_Window* window;
 SDL_Surface* surface;
@@ -126,7 +125,7 @@ int instruction_clear() {
 	}
 
 	//// Create a surface for the screen
-	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	//SDL_Surface* surface = SDL_GetWindowSurface(window);
 	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0)); // Fill the surface with black
 
 	// Draw the entire display to the surface
@@ -143,12 +142,16 @@ int instruction_clear() {
 }
 
 int instruction_subroutine(char secondNibble, char thirdNibble, char fourthNibble) {
-	routine_stack.push(PC);
+	int curr_routine = PC;
+	routine_stack.push(curr_routine);
 	PC = (secondNibble << 8) + (thirdNibble << 4) + (fourthNibble);
 	return PC;
 }
 
 int instruction_subroutine_return() {
+	if (routine_stack.empty()) {
+		return 0;
+	}
 	PC = routine_stack.top();
 	routine_stack.pop();
 	return PC;
@@ -170,7 +173,7 @@ void instruction_skip() {
 
 int instruction_add(char register_num, char thirdNibble, char fourthNibble) {
 	char additive = (thirdNibble << 4) + fourthNibble;
-	registers[register_num] = registers[register_num] + additive;
+	registers[register_num] += additive;
 	return 0;
 }
 
@@ -179,25 +182,82 @@ int instruction_set_index(char secondNibble, char thirdNibble, char fourthNibble
 	return I;
 }
 
+int instruction_bitshift_right(char secondNibble, char thirdNibble) {
+	registers[secondNibble] = registers[thirdNibble];
+	char bit_shifted = registers[secondNibble] & 1;
+	if (bit_shifted == 1) {
+		registers[15] = 1;
+	}
+	else {
+		registers[15] = 0;
+	}
+	registers[secondNibble] = registers[secondNibble] >> 1;
+	return 0;
+}
+
+int instruction_bitshift_left(char secondNibble, char thirdNibble) {
+	registers[secondNibble] = registers[thirdNibble];
+	char bit_shifted = registers[secondNibble] & 128;
+	if (bit_shifted == 1) {
+		registers[15] = 1;
+	}
+	else {
+		registers[15] = 0;
+	}
+	registers[secondNibble] = registers[secondNibble] << 1;
+	return 0;
+}
+int instruction_store(char secondNibble) {
+	for (int i = 0; i <= secondNibble; i++) {
+		memory[I + i] = registers[i];
+	}
+	return 0;
+}
+
+int instruction_load(char secondNibble) {
+	for (int i = 0; i <= secondNibble; i++) {
+		registers[i] = memory[I + i];
+	}
+	return 0;
+}
+
+int instruction_dec_convert(char secondNibble) {
+	int num = registers[secondNibble];
+
+	unsigned char hundreds = num / 100;    // Calculate the hundreds digit
+	unsigned char tens = (num / 10) % 10;  // Calculate the tens digit
+	unsigned char units = num % 10;        // Calculate the units digit
+	
+	memory[I] = hundreds;
+	memory[I + 1] = tens;
+	memory[I + 2] = units;
+
+	return I;
+}
+
 int instruction_draw(char x_coor, char y_coor, char n) {
 	int sprite_start = I; //memory address where sprite begins
 	char length = n;
-	char x = (registers[x_coor]) & 0b00111111; //clips at 64
-	char y = (registers[y_coor]) & 0b00011111; //clips at 32
+	char x = (registers[x_coor]); //x coordinate where sprite will start
+	char y = (registers[y_coor]); //y coordinate where sprite will start 
+	char x__coor = 0;
+	char y__coor = 0;
 
 	// Create a surface for the screen
-	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 255, 255, 255)); // Fill the surface with black
+	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0)); // Fill the surface with black
 
 	// starting at x200, read bit of byte and set it to coordinate
 	for (int row = 0; row < length; row++) {
 		// move on to next byte and y coordinate
 		for (int col = 0; col < 8; col++) {
 			char current_bit = (((memory[I + (row)] << col) & 0x80) >> 7);
-			current_bit = current_bit ^ (display[x + col][y + row]);
-			if ((display[x + col][y + row] == 1) && (current_bit == 0)) {
+			current_bit = current_bit ^ (display[y + row][x + col]);
+			if ((display[y + row][x + col] == 1) && (current_bit == 0)) {
 				registers[15] = 1;
 			}
-			display[x + col][y + row] = current_bit;
+			x_coor = (x + col) & 0b00111111; //clips at 64
+			y_coor = (y + row) & 0b00011111; //clips at 32
+			display[y_coor][x_coor] = current_bit;
 		}
 	}
 
@@ -205,12 +265,12 @@ int instruction_draw(char x_coor, char y_coor, char n) {
 	for (int x = 0; x < SCREEN_WIDTH; x++) {
 		for (int y = 0; y < SCREEN_HEIGHT; y++) {
 			SDL_Rect pixelRect = { x * 10, y * 10, 10, 10 }; // Create a rectangle for the pixel
-			if (display[x][y] == 1) {
-				SDL_FillRect(surface, &pixelRect, SDL_MapRGB(surface->format, 0, 0, 0)); // Draw a black pixel (inverted display)	
+			if (display[y][x] == 1) {
+				SDL_FillRect(surface, &pixelRect, SDL_MapRGB(surface->format, 255, 255, 255)); // Draw a white pixel 	
 			}
 			else {
 				SDL_GetError();
-				SDL_FillRect(surface, &pixelRect, SDL_MapRGB(surface->format, 255, 255, 255)); // Draw a white pixel
+				SDL_FillRect(surface, &pixelRect, SDL_MapRGB(surface->format, 0, 0, 0)); // Draw a black pixel
 			}
 		}
 	}
@@ -226,6 +286,8 @@ int instruction_draw_debug(char x_coor, char y_coor, char n) {
 	char length = n;
 	char x = registers[x_coor];
 	char y = registers[y_coor];
+	char x__coor = 0; 
+	char y__coor = 0;
 
 	// starting at x200, read bit of byte and set it to coordinate
 	for (int row = 0; row < length; row++) {
@@ -236,7 +298,9 @@ int instruction_draw_debug(char x_coor, char y_coor, char n) {
 			if ((display[x + col][y + row] == 1) && (current_bit == 0)) {
 				registers[15] = 1;
 			}
-			display[x + col][y + row] = current_bit;
+			x_coor = (x + col) & 0b00011111; //clips at 64
+			y_coor = (y + row) & 0b00111111; //clips at 32
+			display[x_coor][y_coor] = current_bit;
 		}
 	}
 
@@ -255,12 +319,12 @@ int instruction_draw_debug(char x_coor, char y_coor, char n) {
 	return 0;
 }
 
-int decode_instruction(int instruction) {
+int decode_instruction(int instruction, int PC) {
 	unsigned char firstNibble = (instruction & 0xF000) >> 12;
 	unsigned char secondNibble = (instruction & 0x0F00) >> 8;
 	unsigned char thirdNibble = (instruction & 0x00F0) >> 4;
 	unsigned char fourthNibble = (instruction & 0x000F);
-	int sum, difference;
+	int sum = 0, difference = 0;
 
 	printf("%x%x%x%x\n", firstNibble, secondNibble, thirdNibble, fourthNibble);
 
@@ -286,9 +350,9 @@ int decode_instruction(int instruction) {
 	case 0x1:
 		instruction_jump(secondNibble, thirdNibble, fourthNibble);
 		break;
-	
+
 	case 0x2:
-		instruction_subroutine(secondNibble, thirdNibble, fourthNibble);	
+		instruction_subroutine(secondNibble, thirdNibble, fourthNibble);
 		break;
 
 	case 0x3:
@@ -343,7 +407,7 @@ int decode_instruction(int instruction) {
 				registers[15] = 0;
 			}
 			break;
-		
+
 		case 0x5:
 			difference = registers[secondNibble] - registers[thirdNibble];
 			if (registers[secondNibble] > registers[thirdNibble]) {
@@ -353,10 +417,15 @@ int decode_instruction(int instruction) {
 				registers[15] = 0;
 			}
 			registers[secondNibble] = difference;
-		
+			break;
+
+		case 0x6:
+			instruction_bitshift_right(secondNibble, thirdNibble);
+			break;
+
 		case 0x7:
 			difference = registers[thirdNibble] - registers[secondNibble];
-			
+
 			if (registers[thirdNibble] > registers[secondNibble]) {
 				registers[15] = 1;
 			}
@@ -364,6 +433,11 @@ int decode_instruction(int instruction) {
 				registers[15] = 0;
 			}
 			registers[secondNibble] = difference;
+			break;
+
+		case 0xe:
+			instruction_bitshift_left(secondNibble, thirdNibble);
+			break;
 
 		default:
 			break;
@@ -381,8 +455,24 @@ int decode_instruction(int instruction) {
 		break;
 
 	case 0xD:
-		instruction_draw_debug(secondNibble, thirdNibble, fourthNibble);
+		instruction_draw(secondNibble, thirdNibble, fourthNibble);
 		break;
+
+	case 0xF:
+		switch (thirdNibble)
+		{
+		case 0x3:
+			instruction_dec_convert(secondNibble);
+			break;
+		case 0x5:
+			instruction_store(secondNibble);
+			break;
+		case 0x6:
+			instruction_load(secondNibble);
+			break;
+		default:
+			break;
+		}
 	}
 
 	return 0;
@@ -391,13 +481,14 @@ int decode_instruction(int instruction) {
 
 int main(int argc, char* argv[]) {
 	int startAddress = 0x200;
-	int PC = startAddress;
-	int instruction;
+	PC = startAddress;
+	int instruction = 0;
 
 	//int sizeOfRom = loadRom("IBMLogo.ch8");
 	//int sizeOfRom = loadRom("3-corax+.ch8");
-	int sizeOfRom = loadRom("test_opcode.ch8");
+	//int sizeOfRom = loadRom("test_opcode.ch8");
 	//int sizeOfRom = loadRom("Chip8_Picture.ch8");
+	int sizeOfRom = loadRom("Pong.ch8");
 
 
 	initFont();
@@ -423,10 +514,10 @@ int main(int argc, char* argv[]) {
 			if (PC < startAddress + sizeOfRom) {
 				// Fetch instruction
 				instruction = (memory[PC] << 8) + (memory[PC + 1]);
-				PC = PC + 2; //next instruction
-
+				cout << PC << " ";
 				// Decode instruction
-				decode_instruction(instruction);
+				PC = PC + 2; //next instruction
+				decode_instruction(instruction, PC);
 			}
 
 		}
