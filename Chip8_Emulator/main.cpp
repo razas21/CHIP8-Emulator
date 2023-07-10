24f9d7b37a7d16ab35266421dc49f12185a67fe4
razaa <razaa@XPS-ALI>
2023-07-10 00:15:24 -0400

    finished implementing all necassary opcodes, restructured code, fixed bugs

--- Chip8_Emulator/main.cpp
+++ Chip8_Emulator/main.cpp
@@ -1,12 +1,14 @@
-#include <string>
-using namespace std;
 #include <stdio.h>
-#include <SDL.h>
-#include <SDL_image.h>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
 #include <stack>
+#include <random>
+#include <ctime>
 
+using namespace std;
 
 unsigned char memory[4096] = {0}; //4096 bytes of RAM
 unsigned char registers[16] = {0}; //16 8-bit general purpose registers
@@ -19,6 +21,7 @@ int I = 0; //index register - points at locations in memory
 stack<int> routine_stack; //Used to store the address that the interpreter should return to when finished with a subroutine
 
 int frequency = 700;
+const char* keyPressed = nullptr;
 const int SCREEN_WIDTH = 64;
 const int SCREEN_HEIGHT = 32;
 int display[SCREEN_HEIGHT][SCREEN_WIDTH] = { 0 }; 
@@ -84,6 +87,47 @@ int initFont() {
 	return startAddress;
 }
 
+char decode_keypress(char keyPress) {
+	switch (keyPress) {
+	case '0':
+		return 0x0;
+	case '1':
+		return 0x1;
+	case '2':
+		return 0x2;
+	case '3':
+		return 0x3;
+	case '4':
+		return 0x4;
+	case '5':
+		return 0x5;
+	case '6':
+		return 0x6;
+	case '7':
+		return 0x7;
+	case '8':
+		return 0x8;
+	case '9':
+		return 0x9;
+	case 'A':
+		return 0xA;
+	case 'B':
+		return 0xB;
+	case 'C':
+		return 0xC;
+	case 'D':
+		return 0xD;
+	case 'E':
+		return 0xE;
+	case 'F':
+		return 0xF;
+	default: 
+		//Any non-keypad (hexadecimal) key
+		return 'N';
+		break;
+	}
+}
+
 void destroyDisplay()
 {
 	//Deallocate surface
@@ -101,8 +145,9 @@ void destroyDisplay()
 int loadRom(string filename) {
 	ifstream rom_file;
 	int memoryAddress = 0x200; // program starts at 0x200
+	string filePath = "Roms/" + filename;
 
-	rom_file.open(filename, ios::binary | ios::ate);
+	rom_file.open(filePath, ios::binary | ios::ate);
 	if (rom_file.is_open()) {
 		streampos size = rom_file.tellg(); // get file size
 		rom_file.seekg(0, ios::beg); // move to the beginning of the file
@@ -137,7 +182,6 @@ int instruction_clear() {
 	}
 	// Blit the surface to the window
 	SDL_UpdateWindowSurface(window);
-
 	return 0;
 }
 
@@ -159,6 +203,12 @@ int instruction_subroutine_return() {
 
 int instruction_jump(char secondNibble, char thirdNibble, char fourthNibble) {
 	PC = (secondNibble << 8) + (thirdNibble << 4) + (fourthNibble);
+	return PC;
+}
+
+int instruction_jump_w_offset(char secondNibble, char thirdNibble, char fourthNibble) {
+	int value = ((secondNibble << 8) + (thirdNibble << 4) + (fourthNibble)) + registers[0];
+	PC = value;
 	return PC;
 }
 
@@ -235,6 +285,49 @@ int instruction_dec_convert(char secondNibble) {
 	return I;
 }
 
+int instruction_add_to_index(char secondNibble) {
+	int value = registers[secondNibble];
+	I = I + value;
+	return I;
+}
+
+int instruction_font_char(char secondNibble) {
+	int startAddress = 0x50;
+	char _char = registers[secondNibble] & 0xF;
+	I = memory[startAddress + _char * 5];
+	return 0;
+}
+
+int instruction_random(char secondNibble, char thirdNibble, char fourthNibble) {
+	// Create a random number generator engine
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(0, 99);
+
+	// Generate a random number
+	int randomNum = dis(gen);
+	
+	randomNum = randomNum & ((thirdNibble << 4) + (fourthNibble));
+	registers[secondNibble] = randomNum;
+	return 0;
+}
+
+int instruction_get_key(char secondNibble) {
+	if (keyPressed == nullptr) {
+		instruction_skip();
+	}
+	else {
+		char keyPress = decode_keypress((char)*keyPressed);
+		if (keyPress == 'N') {
+			instruction_skip();
+		}
+		else {
+			registers[secondNibble] = keyPress;
+		}
+	}
+	return 0;
+}
+
 int instruction_draw(char x_coor, char y_coor, char n) {
 	int sprite_start = I; //memory address where sprite begins
 	char length = n;
@@ -255,8 +348,8 @@ int instruction_draw(char x_coor, char y_coor, char n) {
 			if ((display[y + row][x + col] == 1) && (current_bit == 0)) {
 				registers[15] = 1;
 			}
-			x_coor = (x + col) & 0b00111111; //clips at 64
-			y_coor = (y + row) & 0b00011111; //clips at 32
+			x_coor = (x + col) & 0b01111111; //clips at 64
+			y_coor = (y + row) & 0b00111111; //clips at 32
 			display[y_coor][x_coor] = current_bit;
 		}
 	}
@@ -274,50 +367,11 @@ int instruction_draw(char x_coor, char y_coor, char n) {
 			}
 		}
 	}
-
 	// Blit the surface to the window
 	SDL_UpdateWindowSurface(window);
-
 	return 0;
 }
 
-int instruction_draw_debug(char x_coor, char y_coor, char n) {
-	int sprite_start = I; //memory address where sprite begins
-	char length = n;
-	char x = registers[x_coor];
-	char y = registers[y_coor];
-	char x__coor = 0; 
-	char y__coor = 0;
-
-	// starting at x200, read bit of byte and set it to coordinate
-	for (int row = 0; row < length; row++) {
-		// move on to next byte and y coordinate
-		for (int col = 0; col < 8; col++) {
-			char current_bit = (((memory[I + (row)] << col) & 0x80) >> 7);
-			current_bit = current_bit ^ (display[x + col][y + row]);
-			if ((display[x + col][y + row] == 1) && (current_bit == 0)) {
-				registers[15] = 1;
-			}
-			x_coor = (x + col) & 0b00011111; //clips at 64
-			y_coor = (y + row) & 0b00111111; //clips at 32
-			display[x_coor][y_coor] = current_bit;
-		}
-	}
-
-	char pixel = 0;
-	for (int x = 0; x < 32; x++) {
-		for (int y = 0; y < 64; y++) {
-			if (display[y][x] == 1) {
-				pixel = '*';
-			}
-			else {
-				pixel = ' ';
-			}
-			cout << pixel;
-		}
-	}
-	return 0;
-}
 
 int decode_instruction(int instruction, int PC) {
 	unsigned char firstNibble = (instruction & 0xF000) >> 12;
@@ -325,10 +379,11 @@ int decode_instruction(int instruction, int PC) {
 	unsigned char thirdNibble = (instruction & 0x00F0) >> 4;
 	unsigned char fourthNibble = (instruction & 0x000F);
 	int sum = 0, difference = 0;
+	char key = 0;
 
-	printf("%x%x%x%x\n", firstNibble, secondNibble, thirdNibble, fourthNibble);
+	//printf("%x%x%x%x\n", firstNibble, secondNibble, thirdNibble, fourthNibble);
 
-	switch (firstNibble)
+   	switch (firstNibble)
 	{
 	case 0x0:
 		switch (thirdNibble)
@@ -453,14 +508,47 @@ int decode_instruction(int instruction, int PC) {
 	case 0xA:
 		instruction_set_index(secondNibble, thirdNibble, fourthNibble);
 		break;
-
+	case 0xB:
+		instruction_jump_w_offset(secondNibble, thirdNibble, fourthNibble);
+		break;
+	case 0xC:
+		instruction_random(secondNibble, thirdNibble, fourthNibble);
+		break;
 	case 0xD:
 		instruction_draw(secondNibble, thirdNibble, fourthNibble);
 		break;
-
+	case 0xE:
+		switch (thirdNibble)
+		{
+		case 0x9: //EX9E
+			if (keyPressed != NULL) {
+				key = decode_keypress((char) *keyPressed);
+				if (key == registers[secondNibble]) {
+					instruction_skip();
+				}
+			}
+			break;
+		case 0xA: //EXA1
+			if (keyPressed != NULL) {
+				key = decode_keypress((char) *keyPressed);
+				if (key != registers[secondNibble]) {
+					instruction_skip();
+				}
+			}
+			break;
+		}
 	case 0xF:
 		switch (thirdNibble)
 		{
+		case 0x0:
+			instruction_get_key(secondNibble);
+			break;
+		case 0x1:
+			instruction_add_to_index(secondNibble);
+			break;
+		case 0x2:
+			instruction_font_char(secondNibble);
+			break;
 		case 0x3:
 			instruction_dec_convert(secondNibble);
 			break;
@@ -483,13 +571,17 @@ int main(int argc, char* argv[]) {
 	int startAddress = 0x200;
 	PC = startAddress;
 	int instruction = 0;
+	double delay;
 
 	//int sizeOfRom = loadRom("IBMLogo.ch8");
 	//int sizeOfRom = loadRom("3-corax+.ch8");
 	//int sizeOfRom = loadRom("test_opcode.ch8");
-	//int sizeOfRom = loadRom("Chip8_Picture.ch8");
-	int sizeOfRom = loadRom("Pong.ch8");
-
+	//int sizeOfRom = loadRom("4-flags.ch8");
+	//int sizeOfRom = loadRom("5-quirks.ch8");
+	//int sizeOfRom = loadRom("6-keypad.ch8");
+	//int sizeOfRom = loadRom("Tetris [Fran Dachille, 1991].ch8");
+	//int sizeOfRom = loadRom("pong.ch8");
+	int sizeOfRom = loadRom("Space Invaders [David Winter].ch8");
 
 	initFont();
 	if (!initDisplay()) {
@@ -498,7 +590,6 @@ int main(int argc, char* argv[]) {
 	else {
 		bool quit = false; //Main loop flag
 		SDL_Event e; //Event handler
-
 		//While application is running
 		while (!quit)
 		{
@@ -506,24 +597,35 @@ int main(int argc, char* argv[]) {
 			while (SDL_PollEvent(&e) != 0)
 			{
 				//User requests quit
-				if (e.type == SDL_QUIT)
+				if (e.type == SDL_QUIT) 
 				{
 					quit = true;
 				}
+				else if (e.type == SDL_KEYDOWN) {
+					SDL_Keycode keyCode = e.key.keysym.sym;
+					keyPressed = SDL_GetKeyName(keyCode);
+				}
+			}
+			if (keyPressed != nullptr) {
+				cout << "\nKey: " << keyPressed << "\n";
 			}
 			if (PC < startAddress + sizeOfRom) {
+				// Delay
+				delay = 0.012;
+				delay *= CLOCKS_PER_SEC;
+				clock_t now = clock();
+				while (clock() - now < delay);
 				// Fetch instruction
 				instruction = (memory[PC] << 8) + (memory[PC + 1]);
 				cout << PC << " ";
-				// Decode instruction
 				PC = PC + 2; //next instruction
+ 				// Decode instruction
 				decode_instruction(instruction, PC);
+				keyPressed = nullptr;
 			}
-
 		}
 		destroyDisplay();
 	}
-
 	return 0;
 }
 
